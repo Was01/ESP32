6aeddd0742b0a48feffff703f5c5613827fbbfa1
Washington Fernandes de Barros <w_as@yahoo.com>
2023-06-22 20:45:19 -0300

    Nova atualização

---
+++ wifi0/wifi0.ino
@@ -0,0 +1,34 @@
+#include <WiFi.h>
+
+String redeSSID="";
+int intensidadeSinal=-9999;
+
+
+
+void setup() {
+  // put your setup code here, to run once:
+  Serial.begin(115200);
+  Serial.println("Hello, ESP32!");
+  WiFi.mode(WIFI_STA);
+  WiFi.disconnect();
+}
+
+void loop() {
+  int n=WiFi.scanNetworks();
+  Serial.println("Escaneamento realizado");
+  if(n==0){
+    Serial.println("Nenhuma rede encontrada");
+  }else{
+    redeSSID="";
+    intensidadeSinal=-9999;
+    Serial.print(n);
+    Serial.println(" redes encontradas");
+    for(int i=0;i<n;++i){
+      Serial.print(WiFi.SSID(i));
+      Serial.print(": ");
+      Serial.println(WiFi.RSSI(i));
+      delay(1000);
+    }
+  }
+ 
+}
