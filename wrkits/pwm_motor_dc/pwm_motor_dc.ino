55489ce320c2d0437268a7105a9807fe1a8f7b34
Washington Fernandes de Barros <w_as@yahoo.com>
2023-07-30 13:38:43 -0300

    Nova atualização

---
+++ wrkits/pwm_motor_dc/pwm_motor_dc.ino
@@ -0,0 +1,14 @@
+#define motor_dc 5
+
+void setup() {
+  pinMode(motor_dc,OUTPUT);
+  int canal=6;
+  int freq=1000;
+  int resolucao=12;
+  ledcAttachPin(motor_dc,6);
+  ledcSetup(canal,freq,resolucao);
+}
+void loop() {
+  ledcWrite(6,65535);
+
+}
