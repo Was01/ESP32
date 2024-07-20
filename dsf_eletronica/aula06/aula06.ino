3858f7c942130568799ffd92930e15d87a4534b4
Was01 <80862278+Was01@users.noreply.github.com>
2024-07-20 17:58:05 -0300

    Add files via upload

--- dsf_eletronica/aula06/aula06.ino
+++ dsf_eletronica/aula06/aula06.ino
@@ -1,30 +1,26 @@
 #define led1 15
 #define led2 2
 
-#define freq 5000
-#define canal 0
-#define resolucao 10
+int freq=5000;
+int resolucao=8;
 
-void setup()
- {
-
-  ledcSetup (canal,freq,resolucao);
-  ledcAttachPin(led1,canal);
-  ledcAttachPin(led2,canal);
+void setup() {
+    ledcAttach(led1,freq,resolucao);
+    ledcAttach(led2,freq,resolucao);
 
 }
 
-void loop() 
-{
-  for(int i=0;i<1024;i++)
-  {
-    ledcWrite(canal,i);
-    delay(30);
+void loop() {
+  for(int i=0;i<256;i++){
+    ledcWrite(led1,i);
+    ledcWrite(led2,i);
+    delay(50);
   }
-
-  for(int i=1023;i>=0;i--)
-  {
-    ledcWrite(canal,i);
-    delay(30);
+  for(int i=256;i>0;i--){
+    ledcWrite(led1,i);
+    ledcWrite(led2,i);
+    delay(50);
   }
 }
+
+
