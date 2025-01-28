4bf5bf1c2ac58f3eef58475999a49aeb3d08b8d6
Was01 <80862278+Was01@users.noreply.github.com>
2025-01-28 19:40:08 -0300

    Add files via upload

--- dsf_eletronica/aula05/aula05.ino
+++ dsf_eletronica/aula05/aula05.ino
@@ -1,28 +1,28 @@
-
-
 #define led 15
 
 int freq=5000;
-int canal=0;
 int resolucao=8;
 
 
-void setup() {
-  // put your setup code here, to run once:
-  ledcAttachChannel(led,freq,resolucao,canal);
 
+
+void setup() {
+  ledcAttach(led,freq,resolucao);
 }
+  
+
+
 
 void loop() {
-  // put your main code here, to run repeatedly:
-  for(int i=0;i<256;i++){
-   ledcWrite(led,i);
-   delay(30);
-  }
-
-  for(int i=256;i>0;i--){
+  for (int i=0;i<256;i++)
+  {
     ledcWrite(led,i);
     delay(30);
   }
-
+  for(int i=256;i>0;i--)
+  {
+    ledcWrite(led,i);
+    delay(30);
+  }
 }
+
