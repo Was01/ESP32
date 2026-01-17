8ae6b7b4ccbfcc641d384dec3d0abc77916ba7de
Was01 <80862278+Was01@users.noreply.github.com>
2026-01-17 09:30:24 -0300

    Add files via upload

--- dsf_eletronica/aula09/aula09.ino
+++ dsf_eletronica/aula09/aula09.ino
@@ -2,13 +2,13 @@ void setup() {
   // put your setup code here, to run once:
   Serial.begin(115200);
   Serial.println("Hello, ESP32!");
-  pinMode(12, INPUT);
+  pinMode(35, INPUT);
 }
 
 void loop() {
   // put your main code here, to run repeatedly:
- int leitura=analogRead(12);
- Serial.print("A leitura ADC eh: ");
+ int leitura=analogRead(35);
+ Serial.print("\nA leitura ADC eh: ");
  Serial.println((leitura*3.3)/4095);
- delay(500);
+ delay(1000);
 }
