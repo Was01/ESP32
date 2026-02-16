6b861bb1a54c4da4776cde313a8951ee25db91d8
Was01 <80862278+Was01@users.noreply.github.com>
2026-02-16 17:34:38 -0300

    Update ADC.ino

--- senai/ADC/ADC.ino
+++ senai/ADC/ADC.ino
@@ -1,15 +1,25 @@
 
 
-void setup() {
-  pinMode(12,INPUT);
-  Serial.begin(115200);
+#define SENSOR_PIN 35
 
+void setup() {
+  // Aumentamos a estabilidade iniciando o Serial com um pequeno delay
+  Serial.begin(115200);
+  delay(500); 
+  Serial.println("Lendo sensor...");
 }
 
 void loop() {
- int leitura=analogRead(12);
- Serial.print("A leitura ADC eh: ");
- Serial.println(leitura);
- delay(500);
+  int leitura = analogRead(SENSOR_PIN);
+  
+  // Conversão para Voltagem (Considerando 12 bits e 3.3V)
+  float voltagem = (leitura / 4095.0) * 3.3;
 
+  Serial.print("Valor ADC: ");
+  Serial.print(leitura);
+  Serial.print(" | Voltagem: ");
+  Serial.print(voltagem);
+  Serial.println("V");
+
+  delay(500);
 }
